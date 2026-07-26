62350e32124f7162c156f0fe850bddca1fce8d96
Oliviax727 <oliviahrwalters@gmail.com>
2026-07-26 22:30:59 +0800

    incompelte

--- src/Numbers.hs
+++ src/Numbers.hs
@@ -20,15 +20,19 @@ instance Eq R where
     (==) (R an ad ar) (R bn bd br) = (an == bn) && (abs ad == abs bd) && (abs ar == abs br)
 
 instance Ord R where
-    compare (R an ad ar) (R bn bd br) = compare an br
-        --if an != bn then compare an bn else
-        --    if (numberLength ad != numberLength bd) then compare ad br else compare ar br
+    compare (R an ad ar) (R bn bd br)
+      | an /= bn = compare an bn
+      | abs ad /= abs bd = compare (constructDecimal ad) (constructDecimal bd)
+      | otherwise = compare (constructDecimal ar) (constructDecimal br)
 
 instance Real R where
     toRational (R n d r) = toRational n + constructDecimal d + constructFromCyclic r
 
---instance Fractional R where
+-- instance Fractional R where
 --    (/) (R an ad ar) (R bn bd br) =
+
+carryThrough :: Num n => (n -> n -> n) -> (Integer -> Integer) -> (Integer -> Integer)
+carryThrough f a b = if numberLength (f a b) > max (numberLength a) (numberLength b) then 1
 
 instance Num R where
   (+) (R an ad ar) (R bn bd br) = R (an + bn) (ad + bd) (ar + br) -- FIXME
